bdc7867714dcd5117cb7592da4224b0bc76c82e9
MPayne1 <33881180+MPayne1@users.noreply.github.com>
2018-11-09 15:29:25 +0000

    added toolbar with settings option in the overflow

--- app/src/main/java/matt/meetingplanner/MainActivity.java
+++ app/src/main/java/matt/meetingplanner/MainActivity.java
@@ -2,6 +2,9 @@ package matt.meetingplanner;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuInflater;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +12,14 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Toolbar myToolbar = (Toolbar) findViewById(R.id.my_toolbar);
+        setSupportActionBar(myToolbar);
+    }
+
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.bar_items, menu);
+        return true;
     }
 }
+
