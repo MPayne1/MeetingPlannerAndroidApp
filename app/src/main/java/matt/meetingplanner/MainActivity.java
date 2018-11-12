66dea57a2ddc24f53a6aeba4c7d9490e12c8540f
MPayne1 <33881180+MPayne1@users.noreply.github.com>
2018-11-12 10:56:47 +0000

    added date picker dialog

--- app/src/main/java/matt/meetingplanner/MainActivity.java
+++ app/src/main/java/matt/meetingplanner/MainActivity.java
@@ -1,10 +1,27 @@
 package matt.meetingplanner;
 
+import android.app.DatePickerDialog;
+import android.app.Dialog;
+import android.app.DialogFragment;
+import android.icu.util.Calendar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuInflater;
+import android.view.View;
+import android.widget.CalendarView;
+import android.widget.DatePicker;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import java.io.Console;
+import java.text.DateFormat;
+import java.text.FieldPosition;
+import java.text.ParsePosition;
+import java.util.Date;
+import java.util.Locale;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -14,12 +31,36 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         Toolbar myToolbar = (Toolbar) findViewById(R.id.my_toolbar);
         setSupportActionBar(myToolbar);
+        setUpDatePicker();
     }
+
 
     public boolean onCreateOptionsMenu(Menu menu) {
         MenuInflater inflater = getMenuInflater();
         inflater.inflate(R.menu.bar_items, menu);
         return true;
+    }
+
+    // Date needs formatting properly
+    public void setUpDatePicker() {
+        final TextView textDate = (TextView) findViewById(R.id.textDate);
+        final Calendar newCalendar = Calendar.getInstance();
+
+        final DatePickerDialog  StartTime = new DatePickerDialog(this, new DatePickerDialog.OnDateSetListener() {
+            public void onDateSet(DatePicker view, int year, int monthOfYear, int dayOfMonth) {
+                Calendar newDate = Calendar.getInstance();
+                newDate.set(year, monthOfYear, dayOfMonth);
+                textDate.setText(newDate.getTime().toString());
+            }
+        }, newCalendar.get(Calendar.YEAR), newCalendar.get(Calendar.MONTH), newCalendar.get(Calendar.DAY_OF_MONTH));
+
+        textDate.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                StartTime.show();
+
+            }
+        });
     }
 }
 
